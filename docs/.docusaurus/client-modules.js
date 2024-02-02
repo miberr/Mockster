c82a57bb06be43fa07afcffe732746a5008ab325
Mikko <62485576+miberr@users.noreply.github.com>
2024-02-03 01:54:17 +0200

    Visual studio code bugged

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/mikkoberg/Library/Mobile Documents/com~apple~CloudDocs/Documents/GitHub/Mockster/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/mikkoberg/Library/Mobile Documents/com~apple~CloudDocs/Documents/GitHub/Mockster/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/mikkoberg/Library/Mobile Documents/com~apple~CloudDocs/Documents/GitHub/Mockster/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/mikkoberg/Library/Mobile Documents/com~apple~CloudDocs/Documents/GitHub/Mockster/docs/src/css/custom.css"),
-];
