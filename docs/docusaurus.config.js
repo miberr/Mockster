0893397486411d8dd95a042067d907316d898231
Mikko <62485576+miberr@users.noreply.github.com>
2024-02-04 01:29:54 +0200

    App insights fix

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
   plugins: [
     ["@microsoft/docusaurus-plugin-application-insights", {
         config: {
-            instrumentationKey: "InstrumentationKey=709e34cb-daad-4956-a23d-7e276fa0b4e3;IngestionEndpoint=https://westeurope-4.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/",
+          connectionString: "InstrumentationKey=709e34cb-daad-4956-a23d-7e276fa0b4e3;IngestionEndpoint=https://westeurope-4.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/",
         },
         enableClickAnalytics: true,
     }]
